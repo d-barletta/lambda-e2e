3dc838fb695cb7387ec7e1c8477ac3016caccbdf
d.barletta <d.barletta@nsi.it>
2021-02-02 17:21:33 +0100

    working with json payloads

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const chromium = require('chrome-aws-lambda');
- 
+
 const lambdaFunction = async (event, context, callback) => {
   let result = null;
   let browser = null;
   try {
+    if (typeof event === 'string') {
+      event = JSON.parse(event);
+    }
+
     browser = await chromium.puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
@@ -11,17 +15,26 @@ const lambdaFunction = async (event, context, callback) => {
       headless: chromium.headless,
       ignoreHTTPSErrors: true,
     });
+
     ////////////////////////////////////////////////////////////// TEST - BEGIN
-    
+
     let page = await browser.newPage();
-    await page.goto(event.url);
-    result = await page.title();
+    await page.setDefaultTimeout(event.setDefaultTimeout || 30 * 1000);
+    await page.setViewport(event.setViewport || {width: 1280, height: 800});
+    await page.goto(event.goto);
+
+    for (const action of event.actions) {
+      console.log(action);
+      await page[action.execute](...action.params);
+    }
 
     ////////////////////////////////////////////////////////////// TEST - FINISH
+
+    result = {status: 200};
   } catch (error) {
     return callback(error);
   } finally {
-    browser && await browser.close();
+    browser && (await browser.close());
   }
   return callback(null, result);
 };
--- local.js
+++ local.js
@@ -1,3 +1,19 @@
-const {handler} = require("./index");
+const {handler} = require('./index');
+const fs = require('fs');
+const TESTS_DIR = './tests';
 
-handler({url: "https://nsi-demo.forwork.com/"}, {}, console.log);
+fs.readdir(TESTS_DIR, (err, list) => {
+  const jsonFiles = list.filter(name => name.includes('.json'));
+  for (const filename of jsonFiles) {
+    fs.readFile(`${TESTS_DIR}/${filename}`, (err, data) => {
+      handler(data.toString(), {}, (error, result) => {
+        let resp = 'KO';
+        if (result && result.status === 200) {
+          resp = 'OK';
+        }
+        console.log(`${resp} - TEST ${filename.replace('.json', '')}`);
+        error && console.error(error);
+      });
+    });
+  }
+});
